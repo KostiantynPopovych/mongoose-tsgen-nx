4d978c3dbad1ac74cf0bf19e674ad73c21327d0d
personal <kostiantyn.popovych.exp@gmail.com>
2024-07-07 20:02:07 -0700

    Initial commit

---
+++ apps/mongoose-tsgen/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'mongoose-tsgen',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/mongoose-tsgen',
+};
